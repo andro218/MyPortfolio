d605f79bad800030c7491e459d713f9ba55ae7ee
Raben Andro Lagumen <gwapogi1332@gmail.com>
2025-10-19 18:14:35 +0800

    change casing

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Sidebar.css";
+import "./SideBar.css";
 import { Home, User, Folder, Mail } from "lucide-react";
 
 const Sidebar = () => {
